0ebcd82ea9bf17ec609465f8f8f17ec8221718bd
Lucas Veiga Mattos <lucasveigamattos@gmail.com>
2022-08-09 20:03:33 -0300

    Second Commit

--- main.js
+++ main.js
@@ -2,12 +2,7 @@ addEventListener('scroll', onScroll)
 
 function showNavOnScroll() {
     const navigation = document.getElementById('navigation')
-
-    if (scrollY > 0) {
-        navigation.classList.add('scroll')
-    } else {
-        navigation.classList.remove('scroll')
-    }
+    scrollY > 0 ? navigation.classList.add('scroll') : navigation.classList.remove('scroll')
 }
 
 function showBackToTopOnScroll() {
@@ -20,9 +15,29 @@ function showBackToTopOnScroll() {
     }
 }
 
+function activeMenuAtCurrentSection() {
+    const targetLine = scrollY + innerHeight / 2
+
+    const links = document.getElementById('navigation').getElementsByClassName('menu')[0].getElementsByTagName('li')
+
+    const homeTop = document.getElementById('home').offsetTop
+    const homeBottom = document.getElementById('home').offsetHeight
+
+    const servicesTop = document.getElementById('services').offsetTop
+    const servicesBottom = document.getElementById('services').offsetTop + document.getElementById('services').offsetHeight
+
+    const aboutTop = document.getElementById('aboutUs').offsetTop
+    const aboutBottom = document.getElementById('aboutUs').offsetTop + document.getElementById('aboutUs').offsetHeight
+
+    targetLine > homeTop && targetLine < homeBottom ? links[0].classList.add('active') : links[0].classList.remove('active')
+    targetLine > servicesTop && targetLine < servicesBottom ? links[1].classList.add('active') : links[1].classList.remove('active')
+    targetLine > aboutTop && targetLine < aboutBottom ? links[2].classList.add('active') : links[2].classList.remove('active')
+}
+
 function onScroll() {
     showNavOnScroll()
     showBackToTopOnScroll()
+    activeMenuAtCurrentSection()
 }
 
 onScroll()
